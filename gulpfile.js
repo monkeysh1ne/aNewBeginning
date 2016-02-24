3327a0329f08c0f4218a5fd5ceae1e6651a47f92
monkeysh1ne <mcdoob@gmail.com>
2016-02-25 04:03:25 +1300

    Changes to be committed: 	modified:   css/cleanFun.css 	modified:   css/cleanFun.min.css 	modified:   css/colors.min.css 	modified:   css/main.min.css 	modified:   css/reset.min.css 	modified:   gulpfile.js 	modified:   sass/cleanFun.sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var Promise = require('es6-promise').Promise;
 	gulp = require('gulp'),
 	jade = require('gulp-jade'),
 	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
-	csslint = require('gulp-csslint'),
-	browserSync = require('browser-sync').create();
+	prefix = require('gulp-autoprefixer'),
+	// minifycss = require('gulp-minify-css'),
+	// csslint = require('gulp-csslint'),
+	// browserSync = require('browser-sync').create();
 	rename = require('gulp-rename');
 
 // run this task by typing in "gulp jade" in CLI
@@ -18,35 +18,35 @@ gulp.task('jade',function() {
 
 
 // check css syntax for errors and inefficient code
-gulp.task('css', function() {
-	gulp.src('css/*.css')
-	.pipe(csslint())
-	.pipe(csslint.reporter());
-});
+// gulp.task('css', function() {
+// 	gulp.src('css/*.css')
+// 	.pipe(csslint())
+// 	.pipe(csslint.reporter());
+// });
 
 
 // auto-prefixer for cross browser tweaks
 // ...variables
-var autoprefixerOptions = {
-	browsers: ['last 2 versions', '>5%', 'Firefox ESR']
-};
+// var autoprefixerOptions = {
+// 	browsers: ['last 2 versions', '>5%', 'Firefox ESR']
+// };
 
 // run this by typing "gulp styles" in CLI
 gulp.task('styles', function(){ 
 	return sass('sass', { style: 'expanded' })
 		.pipe(gulp.dest('css'))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(autoprefixer(autoprefixerOptions))
+		// .pipe(minifycss())
+		.pipe(prefix('last 2 version'))
 		.pipe(gulp.dest('css'));
 });
 
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "yourlocal.dev"
-    });
-});
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         proxy: "yourlocal.dev"
+//     });
+// });
 
 
 
